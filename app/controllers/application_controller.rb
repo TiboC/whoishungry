334fd832f13432dd51d23b547319abe251c8f36b
Thibaut <thibaut.collette@gmail.com>
2014-06-23 15:33:49 +0200

    Initialize Devise and first page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def flash_class(level)
+    case level
+        when :notice then "info"
+        when :success then "success"
+        when :error, :alert then "danger"
+    end
+  end
+  
 end
