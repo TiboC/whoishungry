43feff6a47ec78f56fcff127b46cebcfbb329f10
Thibaut <thibaut.collette@gmail.com>
2014-06-23 14:27:19 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Areyouhungry::Application.config.secret_key_base = 'd7303ecd7542d91eab81f9153105a3286d4019d8ebe277e121fbab6d1c4149f7f7e450bb1c809a50edbc682d636b707aa4c74395b344f5329c327b56acb59e6a'
